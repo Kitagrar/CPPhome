fd8108ca15f398eb46f53c0452a0a831c394b147
Kitagrar <mangustlimbo@yandex.ru>
2023-09-05 02:08:23 +0300

    costul

--- HW_1/ex_1.cpp
+++ HW_1/ex_1.cpp
@@ -4,7 +4,31 @@ using namespace std;
 
 int main() {
 
-  cout << "Hello, world!";
+string name;
+int price;
+string cash;
+int temp;
 
+ std::cout << "Product's name:";
+ std::cin  >>  name;
+ 
+ std::cout << "Product's price:";
+ std::cin  >>  price;
+ 
+ std::cout << "Is cash-back available for this product? (true/false)";
+ std::cin  >>  cash;
+ 
+ std::cout << "Maximum storing temperature:";
+ std::cin >> temp;
+
+std::cout << name << '\n';
+std::cout << "Price:..........." << "000000" << std::hex << price << '\n';
+std::cout << "Has cash-back:......." << cash << '\n';
+if (temp > 0){
+std::cout << "Max temperature:......+" << temp << '\n';
+}
+else{
+std::cout << "Max temperature:......"  << temp<< '\n';
+}
   return 0;
 }
