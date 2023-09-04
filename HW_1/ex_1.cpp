efea73108ecbbc4a343e95253b107ca13ab4a3c1
Kitagrar <mangustlimbo@yandex.ru>
2023-09-05 02:16:52 +0300

    costyl

--- HW_1/ex_1.cpp
+++ HW_1/ex_1.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <cstring>
-
-using namespace std;
 
 int main() {
 
-string name;
+std::string name;
 int price;
 bool cash;
 int temp;
@@ -17,14 +14,14 @@ int temp;
  std::cin  >>  price;
  
  std::cout << "Is cash-back available for this product? (true/false)";
- std::cin  >>  boolalpha >> cash;
+ std::cin  >>  std::boolalpha >> cash;
  
  std::cout << "Maximum storing temperature:";
  std::cin >> temp;
 
 std::cout << name << '\n';
 std::cout << "Price:..........."<< "000000"  << std::hex << price << '\n';
-std::cout << "Has cash-back:......." << boolalpha << cash << '\n';
+std::cout << "Has cash-back:......." << std::boolalpha << cash << '\n';
 if (temp > 0){
 std::cout << "Max temperature:......+" << std::dec << temp << '\n';
 }
