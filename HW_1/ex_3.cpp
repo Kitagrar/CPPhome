7d041b6a9323d5b9b5345191fcbf5b7594062304
Kitagrar <mangustlimbo@yandex.ru>
2023-09-06 03:08:39 +0300

    quest3

--- HW_1/ex_3.cpp
+++ HW_1/ex_3.cpp
@@ -2,21 +2,20 @@
 #include <math.h>
 
 int main() {
-int a, b, c;
-double dis;
+  int a, b, c;
+  double dis;
 
-std::cin >> a >> b >> c;
+  std::cin >> a >> b >> c;
 
-dis = sqrt(pow(b, 2) - 4 * c * a);
+  dis = sqrt(pow(b, 2) - 4 * c * a);
 
-if (dis > 0) {
-  std::cout << (-float (b) + sqrt(dis)) / 2 * a << (-float (b) - sqrt(dis)) / 2 * a;
-}
-else if (dis == 0) {
-  std::cout << -float (b) / 2 * a;
-}
-else{
-  std::cout << "Корней нет";
-}
+  if (dis > 0) {
+    std::cout << (-float(b) + sqrt(dis)) / 2 * a
+              << (-float(b) - sqrt(dis)) / 2 * a;
+  } else if (dis == 0) {
+    std::cout << -float(b) / 2 * a;
+  } else {
+    std::cout << "Корней нет";
+  }
   return 0;
 }
