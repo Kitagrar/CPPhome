4041ebadf77af958667bf2b9bab3efe5c5dfdef3
Kitagrar <mangustlimbo@yandex.ru>
2023-09-06 03:14:39 +0300

    quest2

---
+++ HW_1/ex_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+  int a, b, c;
+  double dis;
+
+  std::cin >> a, b, c;
+
+  dis = sqrt(pow(b, 2) - 4 * c * a);
+
+  if (dis > 0) {
+    std::cout << (-float(b) + sqrt(dis)) / 2 * a
+              << (-float(b) - sqrt(dis)) / 2 * a;
+  } else if (dis == 0) {
+    std::cout << (-float(b) + sqrt(dis)) / a;
+  } else {
+    std::cout << "Корней нет";
+  }
+  return 0;
+}
