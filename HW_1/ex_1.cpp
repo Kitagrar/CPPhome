934a632d717ce27bdc4b60e931fa8ce2b839f41c
Kitagrar <mangustlimbo@yandex.ru>
2023-09-09 16:17:41 +0300

    ex1

--- HW_1/ex_1.cpp
+++ HW_1/ex_1.cpp
@@ -2,31 +2,32 @@
 
 int main() {
 
-std::string name;
-int price;
-bool cash;
-int temp;
+  std::string name;
+  int price;
+  bool cash;
+  int temp;
+  std::string n;
 
- std::cout << "Product's name:";
- std::cin  >>  name;
- 
- std::cout << "Product's price:";
- std::cin  >>  price;
- 
- std::cout << "Is cash-back available for this product? (true/false)";
- std::cin  >>  std::boolalpha >> cash;
- 
- std::cout << "Maximum storing temperature:";
- std::cin >> temp;
+  std::cout << "Product's name:";
+  std::cin >> name;
 
-std::cout << name << '\n';
-std::cout << "Price:..........."<< "000000"  << std::hex << price << '\n';
-std::cout << "Has cash-back:......." << std::boolalpha << cash << '\n';
-if (temp > 0){
-std::cout << "Max temperature:......+" << std::dec << temp << '\n';
-}
-else{
-std::cout << "Max temperature:......" << std::dec << temp << '\n';
-}
+  std::cout << "Product's price:";
+  std::cin >> price;
+
+  std::cout << "Is cash-back available for this product? (true/false)";
+  std::cin >> std::boolalpha >> cash;
+
+  std::cout << "Maximum storing temperature:";
+  std::cin >> temp;
+
+  std::cout << name << '\n';
+  std::cout << "Price:..........." << std::cout.fill('0') << std::cout.width(8)
+            << std::uppercase << std::hex << price << '\n';
+  std::cout << "Has cash-back:......." << std::boolalpha << cash << '\n';
+  if (temp > 0) {
+    std::cout << "Max temperature:......+" << std::dec << temp << '\n';
+  } else {
+    std::cout << "Max temperature:......" << std::dec << temp << '\n';
+  }
   return 0;
 }
