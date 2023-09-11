0e249226543efbc464cdac1445d2f2b98f3f1994
Snorlax Tanna <snorlaxt@su-e6.msk.21-school.ru>
2023-09-11 21:37:34 +0300

    ex2

--- HW_1/ex_2.cpp
+++ HW_1/ex_2.cpp
@@ -1,21 +1,69 @@
+#include <cmath>
 #include <iostream>
-#include <math.h>
 
 int main() {
-  int a, b, c;
-  double dis;
+  char key;
+  double val = 0.;
+  double g = 0., y = 0., h = 0.;
 
-  std::cin >> a, b, c;
+  std::cout << "Choose the metric to change a)grad --> rad / b)rad-->grad / "
+               "c)H-->kg / d)kg-->H )\n";
+  std::cin >> key >> val;
 
-  dis = sqrt(pow(b, 2) - 4 * c * a);
+  switch (key) {
+  case 'a':
+  case 'A':
+    std::cout << val * M_PI / 180;
+    break;
+  case 'b':
+  case 'B':
+    std::cout << val * 180 / M_PI;
+    break;
+  case 'c':
+  case 'C':
 
-  if (dis > 0) {
-    std::cout << (-float(b) + sqrt(dis)) / 2 * a
-              << (-float(b) - sqrt(dis)) / 2 * a;
-  } else if (dis == 0) {
-    std::cout << (-float(b) + sqrt(dis)) / a;
-  } else {
-    std::cout << "Корней нет";
+    while (true) {
+
+      std::cout << "Enter latitude and height above sea level: ";
+      std::cin >> y >> h;
+      if (std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+        std::cout << "Oops, that input is invalid. Please try again.\n";
+
+      } else {
+        g = 9.780318 * (1 + 0.005302 * pow(sin(y), 2) -
+                        0.000006 * pow(sin(2 * y), 2)) -
+            0.000003086 * h;
+        std::cout << val / g;
+        break;
+      }
+    }
+    break;
+
+  case 'd':
+  case 'D':
+    while (true) {
+
+      std::cout << "Enter latitude and height above sea level: ";
+      std::cin >> y >> h;
+      if (std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+        std::cout << "Oops, that input is invalid. Please try again.\n";
+
+      } else {
+        g = 9.780318 * (1 + 0.005302 * pow(sin(y), 2) -
+                        0.000006 * pow(sin(2 * y), 2)) -
+            0.000003086 * h;
+        std::cout << val * g;
+        break;
+      }
+    }
+    break;
+  default:
+    std::cout << "Oops, that input is invalid. Please try again.\n";
+    break;
   }
   return 0;
 }
