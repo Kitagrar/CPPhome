118c8487f0c6519ef1dbc69d6e9a8f317e8fced9
Snorlax Tanna <snorlaxt@su-e6.msk.21-school.ru>
2023-09-11 21:11:02 +0300

    ex3

--- HW_1/ex_3.cpp
+++ HW_1/ex_3.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 int main() {
   int a = 0, b = 0, c = 0;
   int dis = 0;
 
-  std::cin >> a >> b >> c;
+  while (true) {
+
+    std::cout << "Print a, b, c: ";
+    std::cin >> a >> b >> c;
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(32767, '\n');
+      std::cout << "Oops, that input is invalid. Please try again.\n";
+
+    } else {
+      break;
+    }
+  }
 
   dis = b * b - 4 * c * a;
-  
-  if (a == 0){
-      std::cout << -double(c) / b;
+
+  if (a == 0) {
+    std::cout << -double(c) / b;
   }
 
   else if (dis > 0) {
