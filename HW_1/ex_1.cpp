87b703a2006fd0e03eee71cda15aae1e40e53cfe
Kitagrar <mangustlimbo@yandex.ru>
2023-12-07 21:35:05 +0300

    Bilet_1

--- HW_1/ex_1.cpp
+++ HW_1/ex_1.cpp
@@ -3,30 +3,6 @@
 
 int main() {
 
-  std::string name;
-  int price;
-  bool cash;
-  int temp;
-
-  std::cout << "Product's name:";
-  std::cin >> name;
-
-  std::cout << "Product's price:";
-  std::cin >> price;
-
-  std::cout << "Is cash-back available for this product? (true/false)";
-  std::cin >> std::boolalpha >> cash;
-
-  std::cout << "Maximum storing temperature:";
-  std::cin >> temp;
-
-  std::cout << name << '\n';
-  std::cout << "Price:...........";
-  std::cout.fill('0');
-  std::cout.width(8);
-  std::cout << std::uppercase << std::hex << price << '\n';
-  std::cout << "Has cash-back:......." << std::boolalpha << cash << '\n';
-  std::cout << "Max temperature:......"<< std::noshowpos << std::dec << temp << '\n';
- 
+ std::cout << " Hello world";
   return 0;
 }
--- HW_1/ex_2.cpp
+++ HW_1/ex_2.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 int main() {
-  int a, b, c;
-  double dis;
+char key;
+double val;
+std::cout << "Choose the metric to change( a)C-->K / b)deg-->rad / c)rad-->deg / d)EV-->J / e)Pa --> MOM / g)MOM --> Pa)\n";
+std::cin >> key >> val;
 
-  std::cin >> a, b, c;
 
-  dis = sqrt(pow(b, 2) - 4 * c * a);
+switch(key){
+    case 'a':
+    case 'A':
+    std::cout << val + 273;
+    break;
+    case 'b':
+    case 'B':
+    std::cout << val - 273;
+    break;
+    case 'c':
+    case 'C':
+    std::cout <<  val * M_PI / 180;
+    break;
+    case 'd':
+    case 'D':
+    std::cout << val * 180. / M_PI;
+    break;
+    case 'e':
+    case 'E':
+    std::cout << val * 760. / 101325;
+    break;
+    case 'g':
+    case 'G':
+    std::cout << val * 101325. / 760;
+    break;
+}
 
-  if (dis > 0) {
-    std::cout << (-float(b) + sqrt(dis)) / 2 * a
-              << (-float(b) - sqrt(dis)) / 2 * a;
-  } else if (dis == 0) {
-    std::cout << (-float(b) + sqrt(dis)) / a;
-  } else {
-    std::cout << "Корней нет";
-  }
-  return 0;
+return 0;
 }
